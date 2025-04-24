67eb3c71dca482e80967efdab527ac53705865bf
karandonge072@gmail.com <karandonge072@gmail.com>
2025-04-24 21:05:32 +0530

    added console log

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   test =()=>{
+    console.log("checking changes");
+   }
   return (
     <div className="App">
       <header className="App-header">
