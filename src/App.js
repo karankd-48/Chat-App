f138d9238ca3ca83628f3e17380e5b10181a2827
karandonge072@gmail.com <karandonge072@gmail.com>
2025-04-24 21:12:40 +0530

    Revert "added console log"
    This reverts commit 67eb3c71dca482e80967efdab527ac53705865bf.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-   test =()=>{
-    console.log("checking changes");
-   }
   return (
     <div className="App">
       <header className="App-header">
